dc13f865d39a14433bbbc71e8af154436f57a113
jieguann <60665347+jieguann@users.noreply.github.com>
2022-06-20 00:02:52 -0400

    Add Color change on treeview text

--- Assets/Scripts/TreeViewManager.cs
+++ Assets/Scripts/TreeViewManager.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class TreeViewManager : MonoBehaviour
 {
     [SerializeField] private GameObject treeViewItem;
     [SerializeField] private GameObject parentObject;
+
+    [SerializeField] private List<GameObject> itemList = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
         var itemRoot = Instantiate(treeViewItem, transform);
-        itemRoot.transform.Find("Lable").GetChild(0).GetComponent<Text>().text = gameManager.Instance.testModel.transform.name;
+        itemList.Add(itemRoot);
+        itemRoot.transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().text = "Technical Test Asset";
+            //gameManager.Instance.testModel.transform.name;
         //Debug.Log(gameManager.Instance.testModel.transform.name);
         var parentRoot = Instantiate(parentObject, transform);
 
@@ -19,22 +24,34 @@ public class TreeViewManager : MonoBehaviour
         itemRoot.transform.Find("Arrow").GetComponent<Toggle>().onValueChanged.AddListener(
            delegate { toggleArrow(itemRoot, parentRoot); }
             );
-
+        itemRoot.transform.Find("Button").GetComponent<Button>().onClick.AddListener(delegate { onTextColorChange(itemRoot); }
+            );
         foreach (Transform rootObject in gameManager.Instance.testModel.transform)
         {
             var itemOne = Instantiate(treeViewItem, parentRoot.transform);
-            itemOne.transform.Find("Lable").GetChild(0).GetComponent<Text>().text = rootObject.transform.name;
+            itemList.Add(itemOne);
+            itemOne.transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().text = rootObject.transform.name;
             var parentOne = Instantiate(parentObject, parentRoot.transform);
 
             itemOne.transform.Find("Arrow").GetComponent<Toggle>().onValueChanged.AddListener(
            delegate { toggleArrow(itemOne, parentOne); }
             );
+            itemOne.transform.Find("Button").GetComponent<Button>().onClick.AddListener(delegate { onTextColorChange(itemOne); }
+                        );
             foreach (Transform child in rootObject)
             {
                 var item = Instantiate(treeViewItem, parentOne.transform);
-                item.transform.Find("Lable").GetChild(0).GetComponent<Text>().text = child.transform.name;
+                itemList.Add(item);
+                item.transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().text = child.transform.name;
+                item.transform.Find("Button").GetComponent<Button>().onClick.AddListener(delegate { onTextColorChange(item); }
+                        );
                 item.transform.Find("Arrow").gameObject.SetActive(false);
             }
+        }
+
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            itemList[i].transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().color = Color.black;
         }
     }
 
@@ -51,4 +68,14 @@ public class TreeViewManager : MonoBehaviour
 
         }
     }
+
+    private void onTextColorChange(GameObject item)
+    {
+        for (int i = 0; i < itemList.Count; i++){
+            itemList[i].transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().color = Color.black;
+        }
+        item.transform.Find("Button").GetChild(0).GetComponent<TMP_Text>().color = Color.blue;
+    }
+
+
 }
